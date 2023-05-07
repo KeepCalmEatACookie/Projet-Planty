e645c4dbe17846780d8edf735ebbeb1ae5d00813
KeepCalmEatACookie <sodastream78500@gmail.com>
2023-05-07 20:08:44 +0200

    mise à jour du theme enfant

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -6,11 +6,37 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
-add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
-function add_extra_item_to_nav_menu( $items, $args ) {
-	if (is_user_logged_in()  && $args->menu->slug === 'principal') {
-		$items .= '<li class="menu-item"><a class="menu-link" href=" "> Admin </a></li>';
-	}
-	return $items;
-}
+
+
+/* MODIFICATION DU HEADER */
+
+ // Récupération de la liste des items du menu actuel nom qui est donné au menu dans WP  ocean: "site-header")
+ add_filter( 'wp_nav_menu_items', 'menu_item_admin', 10, 2 );
+ function menu_item_admin ( $items, $args ) {
+		 // Contrôle si l'utilisateur est connecté
+		 if (is_user_logged_in()) {
+			// si l'opérateur est connecté, on initialise un tableau qui stockera la liste des items 
+			$items_array = array();
+			 
+			// Création d'une boucle pour récupérer la position actuel de chaque item 
+			// avec '<li' comme point de repère dans la liste de items
+			// l'offset de 10, permet d'être sûr de ne pas prendre en compte le '<li' où on démarre
+			while ( false !== ( $item_pos = strpos ( $items, '<li', 10 ) ) ) {
+				// recupère uniquement la partie qui va de <li> jusqu'au <li> suivant
+				// et on le stock dans le tableau
+				$items_array[] = substr($items, 0, $item_pos);
+				// puis on retire cette partie de la liste des items avant de recommencer la boucle
+				$items = substr($items, $item_pos);
+			 }
+			 // On ajoute au tableau le dernier élément de la liste
+			 $items_array[] = $items;
+			 // On insère le lien 'Admin' à la position indiquée ici c'est 1 pour qu'il soit en première place dans le tableau.
+			 array_splice($items_array, 1, 0, '<li class="menu-item"><a class="menu-admin" href="'. get_site_url() .'/wp-admin/">Admin</a></li>'); 
+			 // On retransforme le tableau en liste d'items
+			 $items = implode('', $items_array);
+		 }
+		
+		return $items;
+	 
+ }
 ?>
