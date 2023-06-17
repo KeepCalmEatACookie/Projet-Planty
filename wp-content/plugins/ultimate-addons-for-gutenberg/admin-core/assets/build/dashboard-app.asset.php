003972bf3436e5ed6522308e333632c6c1e7de89
KeepCalmEatACookie <sodastream78500@gmail.com>
2023-06-17 20:44:13 +0200

    mise à jour du theme enfant

--- wp-content/plugins/ultimate-addons-for-gutenberg/admin-core/assets/build/dashboard-app.asset.php
+++ wp-content/plugins/ultimate-addons-for-gutenberg/admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'a7fb9d69139827bbce6a8cc7fbdc9ff7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '736d7828de39dd471621');
